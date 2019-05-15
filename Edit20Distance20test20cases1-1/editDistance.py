5021c2eb51a882070795052799d9ebc83af19864
vili1995 <vili.vili1995@gmail.com>
2019-05-16 00:01:40 +0700

    a few update

--- Edit20Distance20test20cases1-1/editDistance.py
+++ Edit20Distance20test20cases1-1/editDistance.py
@@ -1,20 +1,19 @@
-s1 = input()
-s2 = input()
+a = input ()
+b = input ()
 
-# for i in range (len)
-def editDistance(s1, s2):
+def editDistance(s1,s2):
     if len(s1) > len(s2):
         s1, s2 = s2, s1
 
-    distances = range(len(s1) + 1)
-    for i2, c2 in enumerate(s2):
-        distances_ = [i2+1]
-        for i1, c1 in enumerate(s1):
-            if c1 == c2:
-                distances_.append(distances[i1])
-            else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
-        distances = distances_
-    return distances[-1]
 
-print(editDistance(s1,s2))
+    distance1 = range(len(s1) + 1 )
+    for i2,c2 in enumerate(s2):
+        distance2 = [i2+1]
+        for i1,c1 in enumerate(s1):
+            if c1 == c2:
+                distance2.append(distance1[i1])
+            else:
+                distance2.append(1 + min(distance1[i1],distance1[i1+1],distance2[-1]))
+        distance1 = distance2
+    return distance1[-1]
+print (editDistance(a,b))
